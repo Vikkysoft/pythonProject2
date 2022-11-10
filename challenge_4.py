7874ebed7bc88c75320dbc8e54ac7a2902d5e16d
Vikkysoft <bolvic28@gmail.com>
2022-11-10 13:07:11 +0000

    initial commit

---
+++ challenge_4.py
@@ -0,0 +1,10 @@
+# Function that calculate simple interest
+
+def simple_interest(p, r, t):
+    si = p * r * t / 100
+    return si
+
+
+result = simple_interest(1800, 2, 5)
+
+print("The Simple Interest is: ", result)
