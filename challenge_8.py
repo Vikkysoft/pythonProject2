16a1571b54615b20502f8cfa7010dce74ddb8192
Vikkysoft <bolvic28@gmail.com>
2022-11-10 17:14:55 +0000

    initial commit

--- challenge_8.py
+++ challenge_8.py
@@ -1,16 +1,15 @@
-print("\nMy first list:")
+print("\nMy first list")
 my_list1 = [1, 2, 3, 4, 5, 6]
 for i in my_list1:
-    if i % 2 == 0:
+    if i % 2 != 0:
         pass
     else:
         print(i, end=" ")
 
-print("\nMy second list:")
 
+print("\nMy second list")
 my_list2 = [4, 2, 9, 91, 19, 401, 0]
 for i in my_list2:
 
-    if i % 2 != 0:
+    if i % 2 == 0:
         print(i, end=" ")
-
