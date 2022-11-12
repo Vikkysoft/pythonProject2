a40ea33280a0f3c394499c1d26dfae1c5ad03d13
Vikkysoft <bolvic28@gmail.com>
2022-11-12 17:24:15 +0000

    initial commit

--- test_challenge_9_pytest.py
+++ test_challenge_9_pytest.py
@@ -3,6 +3,6 @@ import unittest
 
 
 def test_convert_to_upper_case():
-    assert challenge_9.convert_to_upper_case("testify automation school") == None
-    assert challenge_9.convert_to_upper_case("i have a dog") == print("I HAVE A DOG")
-    assert challenge_9.convert_to_upper_case("i serve a living god") == print("I SERVE A LIVING GOD")
+    assert challenge_9.convert_to_upper_case("testify") == "TESTIFY"
+    assert challenge_9.convert_to_upper_case("i have a dog") == "I HAVE A DOG"
+    assert challenge_9.convert_to_upper_case("i serve a living god") == "I SERVE A LIVING GOD"
