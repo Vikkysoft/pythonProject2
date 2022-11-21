1d6fe363801409e54902cbea26532c5468fef848
Vikkysoft <bolvic28@gmail.com>
2022-11-21 07:19:36 +0000

    second commit

--- challenge_10.py
+++ challenge_10.py
@@ -1,3 +1,10 @@
-#  Create a function that converts any string value to aSentence  case,
+#  Create a function that converts any string value to a Sentence case,
 #  Then  write  a  unit  test  that  checks  the function's correctness.
 
+def convert_string_to_sentence():
+    my_string = "this is a python course for beginners."
+    sentence_string = my_string.title()
+    print(sentence_string)
+
+
+convert_string_to_sentence()
